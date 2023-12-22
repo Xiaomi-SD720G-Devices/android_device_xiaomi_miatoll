3e237a3bf46c83812f2192ddc014ffacfd041252
userariii <abhattacharjee717@gmail.com>
2023-12-22 19:18:48 +0000

    miatoll: Import motorolDolby dependency
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone -b tiramisu+FBEv2 https://github.com/Xiaomi-SD720G-Devices/android_dev
 git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
 git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_sm6250-common.git vendor/xiaomi/sm6250-common --depth=1
 git clone -b miuicam https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miuicamera --depth=1
-git clone -b main https://github.com/userariii/hardware_dolby.git hardware/dolby --depth=1
+git clone -b v1.1 https://github.com/userariii/hardware_motorolaDolby.git hardware/motorolaDolby --depth=1
 
 # clang
 git clone -b clang-r498229b https://github.com/Xiaomi-SD720G-Devices/AOSP-clang.git prebuilts/clang/host/linux-x86/clang-r498229b --depth=1
