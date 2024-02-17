b34449da8099182742452c0236b299289f752f57
userariii <abhattacharjee717@gmail.com>
2024-02-17 23:13:15 +0530

    [TEMP] Revert "sm6250-common: parts: Override HDR types for dolby vision"
    This reverts commit 21e817a1bd7e1d8415ea5f5e6df369600e7c22ed.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,9 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
-import android.os.IBinder;
-import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -39,11 +36,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
-
-        // Override HDR types
-        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
-        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
-                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
-                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
